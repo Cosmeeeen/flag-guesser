b720bf761dc5ef9a499304bdd1d0f1fd0b14dab8
Cosmeeeen <iliemcosmin@yahoo.ro>
2025-05-31 15:04:08 +0300

    cool flag thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { ChangingFlag } from '@/components/changing-flag';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen max-w-md mx-auto">
+      <ChangingFlag className='text-9xl' />
       <h1>Hello world</h1>
       <Button>Test Button</Button>
       <Input placeholder="Test Input" />
